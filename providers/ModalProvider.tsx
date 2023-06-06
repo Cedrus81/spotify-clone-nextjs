9f511343d94900dfd404efe6e6b1d2165315d549
Cedrus81 <erez.eitan.97@gmail.com>
2023-06-06 11:33:25 +0300

    modal and modal provider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Modal from "@/components/Modal"
+import { useEffect, useState } from "react"
+
+function ModalProvider() {
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    if(!isMounted) return null
+
+  return (
+    <Modal />
+  )
+}
+
+export default ModalProvider
